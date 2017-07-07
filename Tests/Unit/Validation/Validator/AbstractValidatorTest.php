8ae95d3b17bf95e5abe533eb83a9075f9078c33b
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-07-07 16:48:49 +0200

    First test structure

---
+++ Tests/Unit/Validation/Validator/AbstractValidatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * AbstractValidatorTest
+ */
+
+namespace TL\Validator\Unit\Validation\Validator;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * AbstractValidatorTest
+ */
+abstract class AbstractValidatorTest extends UnitTestCase
+{
+
+}
--- Tests/Unit/Validation/Validator/LongitudeValidatorTest.php
+++ Tests/Unit/Validation/Validator/LongitudeValidatorTest.php
@@ -6,12 +6,48 @@
 
 namespace TL\Validator\Unit\Validation\Validator;
 
-use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use TL\Validator\Validation\Validator\LongitudeValidator;
 
 /**
  * LongitudeValidatorTest
  */
-class LongitudeValidatorTest extends UnitTestCase
+class LongitudeValidatorTest extends AbstractValidatorTest
 {
 
+
+    /**
+     * @test
+     */
+    public function testValidValues()
+    {
+        $values = [
+            8.748234,
+            '8.748234',
+            16.748234,
+            9
+        ];
+        foreach ($values as $value) {
+            $validator = new LongitudeValidator();
+            $validator->validate($value);
+            $this->assertFalse($validator->hasErrors(), 'Check value: ' . var_export($value, true));
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function testInvalidValues()
+    {
+        $values = [
+            'Text',
+            new \stdClass(),
+            -360.000,
+            190.123123
+        ];
+        foreach ($values as $value) {
+            $validator = new LongitudeValidator();
+            $validator->validate($value);
+            $this->assertTrue($validator->hasErrors(), 'Check value: ' . var_export($value, true));
+        }
+    }
 }
